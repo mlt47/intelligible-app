b615633cd1eff536e5b53d912a291e65c8c5a289
Meurig <mlt47@cam.ac.uk>
2019-08-20 11:59:44 +0100

    Stop webgazer from logging gaze positions to the console

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import '../css/App.css';
 import {Word} from './Word';
 
-function App() {
-    useEffect(() => {
-        window.webgazer.setGazeListener((data, elapsedTime) => {
-            if (data !== null) {
-                console.log(`(${data.x}, ${data.y}) ${elapsedTime}`);
-            }
-        }).begin();
-    }, []);
-
+export const App = () => {
     const text = "A sample sentence for eye tracking";
 
     console.log("Render App");
     return <div className="App">
         <header className="App-header">
             <p>{
-                text.split(" ").map(word => <><Word word={word} />  </>)
+                text.split(" ").map(word => <React.Fragment>
+                    <Word word={word} />{" "}
+                </React.Fragment>)
             }</p>
         </header>
     </div>;
-}
-
-export default App;
+};
